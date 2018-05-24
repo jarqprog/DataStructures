d369ffd87a73d0ea16e53f761b6bb345ba73de59
jarqprog <jarqprog@gmail.com>
2018-05-24 16:12:10 +0200

    implement contains() & findMinimum() with tests

--- src/main/java/com/jarq/binarySearchTree/MyBinarySearchTree.java
+++ src/main/java/com/jarq/binarySearchTree/MyBinarySearchTree.java
@@ -2,16 +2,12 @@ package com.jarq.binarySearchTree;
 
 public class MyBinarySearchTree<T extends Comparable<T>> implements BinarySearchTree<T> {
 
-    private Node<T> root;
+    private Node<T> root = new Node<>(null, null, null);
     private int size = 0;
 
     @Override
     public boolean add(T value) {
-        if(root == null) {
-            root = new Node<>(value, null, null);
-        } else {
-            root.insert(value);
-        }
+        root.insert(value);
         size++;
         return true;
     }
@@ -23,11 +19,15 @@ public class MyBinarySearchTree<T extends Comparable<T>> implements BinarySearch
 
     @Override
     public boolean contains(T value) {
-        return false;
+        return root.find(value) != null;
     }
 
     @Override
     public T findMinimum() {
+        Node<T> node = root.minimum();
+        if(node != null) {
+            return node.data;
+        }
         return null;
     }
 
@@ -38,7 +38,7 @@ public class MyBinarySearchTree<T extends Comparable<T>> implements BinarySearch
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     private class Node<N extends Comparable<N>> {
@@ -53,6 +53,10 @@ public class MyBinarySearchTree<T extends Comparable<T>> implements BinarySearch
         }
 
         private void insert(N value) {
+            if(data == null) {
+                data = value;
+                return;
+            }
             if(data.compareTo(value) >= 0) {
                 if(left == null) {
                     left = new Node<>(value, null, null);
@@ -66,6 +70,28 @@ public class MyBinarySearchTree<T extends Comparable<T>> implements BinarySearch
                     right.insert(value);
                 }
             }
+        }
+
+        private Node<N> find(N value) {
+            if(data == null) {
+                return null;
+            }
+            int factor = data.compareTo(value);
+            if(factor == 0) {
+                return this;
+            }
+
+            else if(factor > 0) {
+                return (left == null) ? null : left.find(value);
+            }
+            return (right == null) ? null : right.find(value);
+        }
+
+        private Node<N> minimum() {
+            if(this.left == null) {
+                return this;
+            }
+            return this.left.minimum();
         }
     }
 }
--- src/test/java/com/jarq/binarySearchTree/MyBinarySearchTreeTest.java
+++ src/test/java/com/jarq/binarySearchTree/MyBinarySearchTreeTest.java
@@ -2,11 +2,22 @@ package com.jarq.binarySearchTree;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 import static org.junit.Assert.*;
 
 public class MyBinarySearchTreeTest {
 
     private BinarySearchTree<Integer> bst = new MyBinarySearchTree<>();
+
+
+    @Test
+    public void size_with_empty_tree() {
+        assertEquals(0, bst.size());
+    }
 
     @Test
     public void add() {
@@ -14,22 +25,144 @@ public class MyBinarySearchTreeTest {
     }
 
     @Test
+    public void size_with_one_element_in_tree() {
+
+        bst.add(20);
+
+        assertEquals(1, bst.size());
+    }
+
+    @Test
+    public void size_with_many_element_in_tree() {
+
+        fillTreeWithInts(50);
+
+        assertEquals(50, bst.size());
+    }
+
+    @Test
+    public void contains_with_empty_tree() {
+        assertFalse(bst.contains(7));
+    }
+
+    @Test
+    public void contains_using_value_which_is_not_in_tree() {
+
+        bst.add(10);
+        bst.add(12);
+        bst.add(7);
+
+        assertFalse(bst.contains(6));
+    }
+
+    @Test
+    public void contains_if_one_element_in_tree() {
+
+        bst.add(10);
+
+        assertTrue(bst.contains(10));
+    }
+
+    @Test
+    public void contains_using_with_three_elements_in_tree() {
+
+        bst.add(10);
+        bst.add(12);
+        bst.add(7);
+
+        assertTrue(bst.contains(12));
+        assertTrue(bst.contains(10));
+    }
+
+    @Test
+    public void contains_using_tree_with_many_elements() {
+
+        fillTreeWithInts(40);
+
+        assertTrue(bst.contains(12));
+        assertTrue(bst.contains(9));
+        assertTrue(bst.contains(30));
+        assertTrue(bst.contains(35));
+        assertTrue(bst.contains(2));
+        assertTrue(bst.contains(19));
+
+        assertFalse(bst.contains(-2));
+        assertFalse(bst.contains(50));
+        assertFalse(bst.contains(99));
+    }
+
+
+    @Test
     public void remove() {
     }
 
     @Test
-    public void contains() {
+    public void findMinimum_with_empty_tree() {
+        assertNull(bst.findMinimum());
     }
 
     @Test
-    public void findMinimum() {
+    public void findMinimum_using_tree_with_five_elements() {
+
+        int expected = -999;
+        bst.add(10);
+        bst.add(809);
+        bst.add(expected);
+        bst.add(0);
+        bst.add(-332);
+
+        assertEquals(expected, (long) bst.findMinimum());
     }
 
     @Test
-    public void findMaximum() {
+    public void findMinimum_using_tree_with_many_elements() {
+
+        fillTreeWithRandomElementsBetweenZeroAndGivenValue(500, 1000);
+        long expected = 0;
+
+        assertEquals(expected, (long) bst.findMinimum());
     }
 
     @Test
-    public void size() {
+    public void findMaximum_with_empty_tree() {
+        assertNull(bst.findMaximum());
     }
+
+    @Test
+    public void findMaximum_using_tree_with_many_elements() {
+
+        int maxValue = 1000;
+        fillTreeWithRandomElementsBetweenZeroAndGivenValue(500, maxValue);
+
+        assertEquals(maxValue, (long) bst.findMaximum());
+    }
+
+    private void fillTreeWithInts(int numberOfElements) {
+        int counter = 0;
+        while(counter < numberOfElements) {
+            bst.add(counter);
+            counter++;
+        }
+    }
+
+    private void fillTreeWithRandomElementsBetweenZeroAndGivenValue(int numberOfElements, int maxValue) {
+
+        Random chaos = new Random();
+        List<Integer> numbers = new ArrayList<>(numberOfElements);
+        int counter = 0;
+        while(counter < numberOfElements-2) {
+            numbers.add(chaos.nextInt(maxValue));
+            counter++;
+        }
+        numbers.add(0);
+        numbers.add(maxValue);
+
+
+        Collections.shuffle(numbers);
+        for(int num : numbers) {
+            bst.add(num);
+        }
+    }
+
+
 }
