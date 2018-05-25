ab41d865bb385214ff6ef5c754d22096b9711c9a
jarqprog <jarqprog@gmail.com>
2018-05-25 19:20:53 +0200

    refactor

--- src/main/java/com/jarq/binarySearchTree/BSTDemo.java
+++ src/main/java/com/jarq/binarySearchTree/BSTDemo.java
@@ -9,87 +9,43 @@ public class BSTDemo {
         List<Integer> numbers = generateRandomNumbers(500, 500);
         BinarySearchTree<Integer> tree = generateTreeWithNumbers(numbers);
         drawTree(tree);
-        benchmark(numbers, tree);
-    }
 
+        // perform benchmark with different setup data:
+        System.out.println("\nPreparing test for list vs tree - testing execution time for " +
+                "methods: contains/add/remove. Results in nanoseconds:\n");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Type something & press <enter> to run tests.. ");
+        sc.nextLine();
+        int[] setupData = {100, 200, 300, 400, 500};
+        for(int data : setupData ) {
+            numbers = generateRandomNumbers(data, data);
+            tree = generateTreeWithNumbers(numbers);
+            benchmark(numbers, tree);
+        }
+    }
 
     private static void drawTree(BinarySearchTree<Integer> tree) {
         BinarySearchTreePrinter printer = new LevelsPrinter<>(tree);
-
         System.out.println("Drawing tree:\n");
         printer.print();
-        System.out.println("******");
+        System.out.println("******\n");
     }
 
     private static void benchmark(List<Integer> numbers, BinarySearchTree<Integer> tree) {
-
-        System.out.println("Preparing test for list vs tree (size: " + tree.size() + ")");
-        System.out.println("\t - test using elements:");
-
         int firstElement = numbers.get(0);
         int middleElement = numbers.get(numbers.size() / 2);
         int lastElement = numbers.get(numbers.size()-1);
 
         int[] elements = {300, 200, 100, 50, 12, firstElement, middleElement, lastElement, -1000, -5, -1};
-        long startTime;
-        long stopTime;
+
+        System.out.println("\n***********************************************");
+        System.out.println("Data structures size: " + tree.size());
+        System.out.println("\t - test using elements:");
         System.out.println("\t" + Arrays.toString(elements));
-        System.out.println("*******************");
 
-        // CONTAINS:
-        System.out.println("\n\t - test CONTAINS() elements:");
-        // list test
-        startTime = System.nanoTime();
-        for(int el : elements) {
-            numbers.contains(el);
-        }
-        stopTime = System.nanoTime() - startTime;
-        System.out.println("List test (CONTAINS): total execution time (in nanos): " + stopTime);
-
-        // tree test
-        startTime = System.nanoTime();
-        for(int el : elements) {
-            tree.contains(el);
-        }
-        stopTime = System.nanoTime() - startTime;
-        System.out.println("BinarySearchTree test (CONTAINS): total execution time (in nanos): " + stopTime);
-
-
-        // ADD:
-        System.out.println("\n\t - test ADD() elements:");
-        // list test
-        startTime = System.nanoTime();
-        for(int el : elements) {
-            numbers.add(el);
-        }
-        stopTime = System.nanoTime() - startTime;
-        System.out.println("List test (ADD): total execution time (in nanos): " + stopTime);
-
-        // tree test
-        startTime = System.nanoTime();
-        for(int el : elements) {
-            tree.add(el);
-        }
-        stopTime = System.nanoTime() - startTime;
-        System.out.println("BinarySearchTree test (ADD): total execution time (in nanos): " + stopTime);
-
-        // REMOVE:
-        System.out.println("\n\t - test REMOVE() elements:");
-        // list test
-        startTime = System.nanoTime();
-        for(Integer el : elements) {
-            boolean isDone = numbers.remove(el);  // use bool as return type, to not removing by index
-        }
-        stopTime = System.nanoTime() - startTime;
-        System.out.println("List test (REMOVE): total execution time (in nanos): " + stopTime);
-
-        // tree test
-        startTime = System.nanoTime();
-        for(Integer el : elements) {
-            boolean isDone = tree.remove(el);
-        }
-        stopTime = System.nanoTime() - startTime;
-        System.out.println("BinarySearchTree test (REMOVE): total execution time (in nanos): " + stopTime);
+        containsTest(numbers, tree, elements);
+        addTest(numbers, tree, elements);
+        removeTest(numbers, tree, elements);
     }
 
     private static BinarySearchTree<Integer> generateTreeWithNumbers(List<Integer> numbers) {
@@ -113,5 +69,66 @@ public class BSTDemo {
 
         Collections.shuffle(numbers);
         return numbers;
+    }
+
+    private static void containsTest(List<Integer> numbers, BinarySearchTree<Integer> tree, int[] elements) {
+        long startTime, stopTime;
+        System.out.println("\n\t - test CONTAINS():");
+        // list test
+        startTime = System.nanoTime();
+        for(int el : elements) {
+            numbers.contains(el);
+        }
+        stopTime = System.nanoTime() - startTime;
+        System.out.print("\tList: " + stopTime);
+
+        // tree test
+        startTime = System.nanoTime();
+        for(int el : elements) {
+            tree.contains(el);
+        }
+        stopTime = System.nanoTime() - startTime;
+        System.out.print(" vs BinarySearchTree: " + stopTime);
+    }
+
+    private static void addTest(List<Integer> numbers, BinarySearchTree<Integer> tree, int[] elements) {
+        long startTime, stopTime;
+        System.out.println("\n\t - test ADD():");
+        // list test
+        startTime = System.nanoTime();
+        for(int el : elements) {
+            numbers.add(el);
+        }
+        stopTime = System.nanoTime() - startTime;
+        System.out.print("\tList: " + stopTime);
+
+        // tree test
+        startTime = System.nanoTime();
+        for(int el : elements) {
+            tree.add(el);
+        }
+        stopTime = System.nanoTime() - startTime;
+        System.out.print(" vs BinarySearchTree: " + stopTime);
+    }
+
+    private static void removeTest(List<Integer> numbers, BinarySearchTree<Integer> tree, int[] elements) {
+        long startTime, stopTime;
+
+        System.out.println("\n\t - test REMOVE():");
+        // list test
+        startTime = System.nanoTime();
+        for(Integer el : elements) {
+            boolean isDone = numbers.remove(el);  // use bool as return type, to not removing by index
+        }
+        stopTime = System.nanoTime() - startTime;
+        System.out.print("\tList: " + stopTime);
+
+        // tree test
+        startTime = System.nanoTime();
+        for(Integer el : elements) {
+            boolean isDone = tree.remove(el);
+        }
+        stopTime = System.nanoTime() - startTime;
+        System.out.print(" vs BinarySearchTree: " + stopTime);
     }
 }
