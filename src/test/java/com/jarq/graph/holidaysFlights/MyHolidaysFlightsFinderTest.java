6e7120870a4ce7ab103f61cccf48a110d74c86ca
jarqprog <jarqprog@gmail.com>
2018-06-06 20:20:25 +0200

    add tests

---
+++ src/test/java/com/jarq/graph/holidaysFlights/MyHolidaysFlightsFinderTest.java
@@ -0,0 +1,24 @@
+package com.jarq.graph.holidaysFlights;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MyHolidaysFlightsFinderTest {
+
+    @Test
+    public void calculateCheapestTravel() {
+    }
+
+    @Test
+    public void getCheapestTravel() {
+    }
+
+    @Test
+    public void calculateShortestTravel() {
+    }
+
+    @Test
+    public void getShortestTravel() {
+    }
+}
